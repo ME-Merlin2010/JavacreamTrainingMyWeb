f15a0ebe9f79785e2ec147579b28ebd3a328e938
Rainer Sawitzki <training@rainer-sawitzki.de>
2017-12-12 09:45:52 +0100

    add order example

---
+++ org.javacream.training.application.books/src/main/java/org/javacream/books/order/impl/SimpleOrderService.java
@@ -0,0 +1,42 @@
+package org.javacream.books.order.impl;
+
+import java.util.Random;
+
+import org.javacream.books.order.api.Order;
+import org.javacream.books.order.api.OrderService;
+import org.javacream.books.order.api.OrderStatus;
+import org.javacream.books.warehouse.api.BooksService;
+import org.javacream.store.api.StoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SimpleOrderService implements OrderService{
+
+	@Autowired
+	private StoreService storeService;
+	@Autowired
+	private BooksService booksService;
+	private Random random = new Random(this.hashCode() + System.currentTimeMillis());
+
+	@Override
+	public Order order(String isbn, int number) {
+		long orderId = random.nextLong();
+		OrderStatus status;
+		double totalPrice = 0;
+		try {
+			double price = booksService.findBookByIsbn(isbn).getPrice();
+			totalPrice = number * price;
+			int stock = storeService.getStock("books", isbn);
+			if (stock < number) {
+				status = OrderStatus.PENDING;
+			} else {
+				status = OrderStatus.OK;
+			}
+		} catch (Exception e) {
+			status = OrderStatus.UNAVAILABLE;
+		}
+		return new Order(orderId, isbn, totalPrice, status);
+	}
+
+}
