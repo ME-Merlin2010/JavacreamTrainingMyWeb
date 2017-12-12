10ad0b54e1c227f748a1ea77aca3732bda53eb62
Rainer Sawitzki <training@rainer-sawitzki.de>
2017-12-12 12:10:46 +0100

    implement JdbcStoreService
    remove SimpleStoreService

--- org.javacream.training.application.books/src/main/java/org/javacream/books/BooksApplicationConfig.java
+++ org.javacream.training.application.books/src/main/java/org/javacream/books/BooksApplicationConfig.java
@@ -1,23 +1,18 @@
 package org.javacream.books;
 
-import org.javacream.store.api.StoreService;
-import org.javacream.store.impl.SimpleStoreService;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class BooksApplicationConfig {
-	@Value("${stock}")
-	private int stock;
-
-	@Bean
-	@Scope("singleton")
-	public StoreService storeService() {
-		SimpleStoreService simpleStoreService = new SimpleStoreService();
-		simpleStoreService.setStock(stock);
-		return simpleStoreService;
-	}
+//	@Value("${stock}")
+//	private int stock;
+//
+//	@Bean
+//	@Scope("singleton")
+//	public StoreService storeService() {
+//		JdbcStoreService simpleStoreService = new JdbcStoreService();
+//		simpleStoreService.setStock(stock);
+//		return simpleStoreService;
+//	}
 
 }
--- org.javacream.training.application.books/src/test/java/org/javacream/books/warehouse/test/BooksServiceTest.java
+++ org.javacream.training.application.books/src/test/java/org/javacream/books/warehouse/test/BooksServiceTest.java
@@ -1,14 +1,20 @@
 package org.javacream.books.warehouse.test;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.sql.DataSource;
 
 import org.javacream.books.warehouse.api.BooksService;
+import org.javacream.store.api.StoreService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.StatementCallback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -27,18 +33,41 @@ public class BooksServiceTest {
 	@Autowired
 	private BooksService booksService;
 
-	@Autowired DataSource dataSource;
+	@Autowired private StoreService storeService;
+	@Autowired
+	DataSource dataSource;
+
+	@Autowired private JdbcTemplate jdbcTemplate;
 	@Test
-	
 	public void testSpring() {
 		TestActor.doTest(booksService);
 	}
 
-	@Test public void testDataSource() throws Exception{
+	@Test
+	public void testDataSource() throws Exception {
 		Connection con = dataSource.getConnection();
 		Assert.assertNotNull(con);
 		con.close();
 	}
-	
 
+	@Test
+	public void testJdbcTemplate() {
+		Assert.assertNotNull(jdbcTemplate);
+		String result = jdbcTemplate.execute(new StatementCallback<String>() {
+			@Override
+			public String doInStatement(Statement statement) throws SQLException, DataAccessException {
+				statement.execute("drop table messages if exists");
+				statement.execute("create table messages (message varchar(256))");
+				statement.execute("insert into messages (message) values ('Hello')");
+				return "OK";
+			}
+		});
+		Assert.assertEquals("OK", result);
+	}
+	
+	@Test public void testStoreService() {
+		Assert.assertEquals(42, storeService.getStock("books", "ISBN1"));
+		Assert.assertEquals(200, storeService.getStock("books", "ISBN3"));
+		Assert.assertEquals(0, storeService.getStock("irgend", "was"));
+	}
 }
